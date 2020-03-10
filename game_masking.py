06f302dca068fb4664e7a046428eeabf8aaf5383
quart_cavestory <jangminjun6014@gmail.com>
2020-03-11 01:52:45 +0900

    First operating version
    grasstown의 작은 크리터 추척 가능, 그러나 화면 상에 존재 하면 자동으로 사격 활성화만 함으로 미완성 단계임

--- game_masking.py
+++ game_masking.py
@@ -5,7 +5,7 @@ from PIL import ImageGrab
 # for keyborad configuration
 import pyautogui as pg
 import time as t
-import keypress as kp
+import threading as th
 
 window_x = 0
 window_y = 50
@@ -22,6 +22,8 @@ upper_red = (4, 214, 255)
 lower_red = (4, 214, 255)
 shootFlag = False
 
+temp = 0
+enemyinScreen = False
 
 x_tot = 0
 y_tot = 0
@@ -53,11 +55,17 @@ def autoMove(x_tot, y_tot, img):
 
 
 def drawRect(maskingImage, colorImage, heroFlag=True):
-    global x_tot, y_tot, index
+    global x_tot, y_tot, index, temp, enemyinScreen
     # 파이썬에서 추적한 물체에 대한 정보를 주는 함수, 이것으로 쉽게 박스를 그릴 수 있다.
     numOfLables, img_label, stats, centroids = cv2.connectedComponentsWithStats(
         maskingImage
     )
+    if heroFlag == False:
+        if numOfLables >= 2:
+            enemyinScreen = True
+        else:
+            enemyinScreen = False
+        # print("enemyinScreen : ", enemyinScreen)
     for idx, centroid in enumerate(centroids):
 
         if stats[idx][0] == 0 and stats[idx][1] == 0:
@@ -74,9 +82,13 @@ def drawRect(maskingImage, colorImage, heroFlag=True):
                 y_tot += y
                 index += 1
         else:
+            # it means it detected an enemy
             if area > 0:
                 cv2.circle(colorImage, (centerX, centerY), 5, (0, 0, 255), 1)
                 cv2.rectangle(colorImage, (x, y), (x + width, y + height), (0, 0, 255))
+                if x > x_tot - 100 and x < x_tot + 100:
+                    if abs(x_tot - temp) > abs(x_tot - x):
+                        temp = x
 
     if heroFlag:
         if index == 0:
@@ -100,46 +112,60 @@ def drawRect(maskingImage, colorImage, heroFlag=True):
         # autoMove(x_tot, y_tot, img_color)
 
 
-def safeRect(colorImg, weaponRange=450, jumpHeight=300):
+def safeRect(colorImg, weaponRange=100, jumpHeight=300):
     global shootFlag
+    x_near = temp
     distance = abs(x_tot - x_near)
-    lv1 = weaponRange
-    lv2 = weaponRange / 3 * 2
+    # print("distance : %d" % distance)
 
-    if distance <= lv2:
-        cv2.rectangle(
-            colorImg,
-            (x_tot - int(lv2 / 2), y_tot + 40),
-            (x_tot + int(lv2 / 2), y_tot - 40),
-            (100, 234, 231),
-            1,
-        )
-        shootFlag = True
-    elif distance <= lv1:
+    if enemyinScreen == True:
+        if distance <= weaponRange:
+            shootFlag = True
+            # print(1)
+        else:
+            shootFlag = False
+            # print(2)
+    else:
+        shootFlag = False
+        # print(3)
+    # print("shootFlag : ", shootFlag)
+
+
+# print("x_tot : %d" % x_tot)
+# print("x_near : %d" % x_near)
+# print("distance : %d" % distance)
+"""
+    if distance <= lv1:
         cv2.rectangle(
             colorImg,
             (x_tot - int(lv1 / 2), y_tot + 40),
             (x_tot + int(lv1 / 2), y_tot - 40),
-            (0, 231, 234),
+            (100, 234, 231),
             1,
         )
-        shootFlag = True
+        if enemyinScreen == True:
+            shootFlag = True
+        else:
+            shootFlag = False
     else:
         shootFlag = False
     print(shootFlag)
+"""
 
 
 def vrbRef():
-    global x_tot, y_tot, index
+    global x_tot, y_tot, index, x_near
     x_tot = 0
     y_tot = 0
+    x_near = 0
 
     index = 0
+    temp = 1000
 
 
-def main():
+def Kmain():
 
-    kp.Messenger(name="a").setdata(2).start()
+    # kp.Messenger(name="a").setdata(2).start()
 
     while True:
         img_original = ImageGrab.grab(
@@ -169,5 +195,133 @@ def main():
     cv2.destroyAllWindows()
 
 
-main()
+Kmain()
 
+
+time_count = 0
+fb_Maxarmo = 4
+
+
+def jump():
+    pg.keyDown("z")
+    t.sleep(1)
+    pg.keyUp("z")
+
+
+def autoMoveRight():
+    pg.keyDown("right")
+    t.sleep(2)
+
+
+def shootOnce():
+    pg.keyDown("x")
+    t.sleep(0.05)
+    pg.keyUp("x")
+    t.sleep(0.05)
+
+
+def switchWeapon(direction="right"):
+    if direction == "right":
+        pg.keyDown("s")
+        t.sleep(0.05)
+        pg.keyUp("s")
+
+
+def autoShoot(weapon="normal", armo=1, comboFlag=False, weaponNum=3):
+    if comboFlag == True:
+        # 가장 먼저 파이어볼로 조진다
+        print("combo acvd")
+        for _ in range(weaponNum):
+            for i in range(armo):
+                shootOnce()
+            switchWeapon("right")
+    elif comboFlag == False:
+        if weapon == "normal":
+            # print("ploar star autoShoot")
+            for i in range(armo):
+                shootOnce()
+                # print(i)
+
+
+def autoMoveJet(direction="right"):
+    pg.keyDown("z")
+    t.sleep(0.05)
+    pg.keyUp("z")
+    t.sleep(0.05)
+    pg.keyDown(direction)
+    t.sleep(0.02)
+    pg.keyDown("z")
+    t.sleep(1.5)
+    pg.keyUp(direction)
+    pg.keyUp("z")
+
+
+def autoMove(direction="left"):
+    pg.keyDown(direction)
+
+
+pg.PAUSE = 0
+
+# autoShoot()
+
+prevTime = 0
+
+
+class Messenger(th.Thread):
+    def setdata(self, num):
+        self.num = num
+
+    def run(self):
+        global prevTime
+        if self.num == 4:
+            Kmain()
+        while True:
+            # print("while open %d" % self.num)
+            if prevTime < time_count:
+                print("timecont")
+                prevTime = time_count
+            if self.num == 2:
+                if shootFlag == True:
+                    print("shoot")
+                    shootOnce()
+                print(4)
+            elif self.num == 3:
+                print("jump")
+                jump()
+            else:
+                continue
+            if time_count > 20:
+                print("ends!")
+                break
+        pg.keyUp("right")
+
+
+a = Messenger(name="a")
+b = Messenger(name="b")
+c = Messenger(name="c")
+d = Messenger(name="d")
+
+
+def startTimer():
+    global time_count
+    time_count += 1
+    print("time : %d" % time_count)
+    timer = th.Timer(1, startTimer)
+    timer.start()
+
+    if time_count > 10:
+        print("stop")
+        timer.cancel()
+
+
+startTimer()
+
+
+# a.setdata(1)  # 1 is for automove
+b.setdata(2)
+# c.setdata(3)
+d.setdata(4)
+
+# a.start()
+b.start()
+d.start()
